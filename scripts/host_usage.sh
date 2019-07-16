eed67a3b7847badf65cbbde8d614ce659bc2f0d5
shyleshragun <51927842+shyleshragun@users.noreply.github.com>
2019-07-15 22:03:15 -0400

    Update host_usage.sh
    removed extra lines

--- scripts/host_usage.sh
+++ scripts/host_usage.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-
 #arguments declaration
 psql_host=$1
 psql_port=$2
@@ -8,20 +7,13 @@ db_name=$3
 psql_user=$4
 psql_password1=$5
 
-
-
-
 get_memory_free(){
 	memory_free=$(vmstat -t | head -n3 | tail -n1 | awk '{print $4}') 
-
 }
-
 
 get_cpu_idel(){
 	cpu_idel=$( vmstat -t | head -n3 | tail -n1 | awk '{print $15}')
-
 }
-
 
 get_cpu_kernel(){
 	cpu_kernel=$(vmstat -t | head -n3 | tail -n1 | awk '{print $14}')
@@ -29,17 +21,13 @@ get_cpu_kernel(){
 
 get_disk_io(){
 	disk_io=$(vmstat -d | head -n3 | tail -n1 | awk '{print $10}')
-
 }
 
 get_disk_available(){
 	disk_available=$(df -BM / | head -n3 | tail -n1 | awk '{print $4}' | sed 's/M//')
-
 }
 
-
 #STEP1: parse data and setup variables
-
 get_memory_free
 get_cpu_idel
 get_cpu_kernel
@@ -47,10 +35,8 @@ get_disk_io
 get_disk_available
 timestamp=$(date +"%Y-%m-%d %H:%M:%S")
 
-
 #STEP2: SAVE HOSTID 
 host_id=$(cat ~/host_id)
-
 
 #STEP3: construct INSERT statement
 insert_stmt=$(cat <<-END
@@ -59,13 +45,7 @@ END
 )
 echo $insert_stmt
 
-
-
-
 #STEP4: EXECUTE INSERT STATEMENT
 export PGPASSWORD=$psql_password1
 psql -h $psql_host -p $psql_port -U $psql_user -d $db_name -c "$insert_stmt"
 sleep 1
-
-
-
